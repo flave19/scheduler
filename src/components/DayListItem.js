81a8b7cb722e0a56e8733603b4db42bc627b6c88
Flavian <flavian.mohanraj@gmail.com>
2020-02-18 18:39:41 -0500

    listing days 2 and 3 complete

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,20 +1,32 @@
 import React from "react";
 
-import classNames from "classnames"
+import classNames from "classnames";
 
-import "components/DayListItem.scss"
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
   const dayClass = classNames({
     "day-list__item": true,
-    "day-list__item--selected":props.selected,
-    "day-list__item--full":props.spots === 0
-  }) 
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
 
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
-  )
+  );
+}
+
+function formatSpots(spots) {
+  if (spots === 0) {
+    return "no spots remaining";
+  } 
+  else if (spots === 1) {
+    return `${spots} spot remaining`;
+  } 
+  else{
+    return `${spots} spots remaining`;
+  }
 }
