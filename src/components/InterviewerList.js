1f6542c36b7a5af7c0f0a2631c5d6e99397e3a1e
Flavian <flavian.mohanraj@gmail.com>
2020-02-19 12:13:47 -0500

    interviewers 2 complete

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewList.scss";
+
+import { action } from "@storybook/addon-actions/dist/preview";
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map((interviewer, index) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={event => props.setInterviewer(interviewer.id)}
+      />
+    );
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  );
+}
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,15 +4,20 @@ import classNames from "classnames";
 
 import "components/InterviewerListItem.scss";
 
-export default function InterviewerListItem(props){
-  return(
-  <li className="interviewers__item">
-  <img
-    className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
-  />
-  Sylvia Palmer
-</li>
-  )
+export default function InterviewerListItem(props) {
+  const interviewerClass = classNames({
+    interviewers__item: true,
+    "interviewers__item--selected": props.selected
+  });
+
+  return (
+    <li className={interviewerClass} onClick={props.setInterviwer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
 }
