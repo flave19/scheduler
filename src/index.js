ec9a92b4add85b07b6fe26ea40ac9981d8740867
Flavian <flavian.mohanraj@gmail.com>
2020-03-02 12:43:38 -0500

    added scheduler api index info

--- src/index.js
+++ src/index.js
@@ -12,3 +12,16 @@ if (process.env.REACT_APP_API_BASE_URL) {
 }
 
 ReactDOM.render(<Application />, document.getElementById("root"));
+
+
+const pg = require("pg");
+
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL || ""
+});
+
+client
+  .connect()
+  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+
+module.exports = client;
